9484246be0b692597082295c8df95161610791f5
Tom <chqteng1234@163.com>
2024-12-27 14:25:51 +0800

    Dec 27 14:25PM

---
+++ mall-common/src/main/java/com/aoding/mall/api/CommonPage.java
@@ -0,0 +1,23 @@
+package com.aoding.mall.api;
+
+import java.util.List;
+
+public class CommonPage<T> {
+
+    private Integer pageNum;
+
+    private Integer pageSize;
+
+    private Integer totalPage;
+
+    private Long total;
+
+    private List<T> list;
+
+//    public static <T> CommonPage<T> restPage(List<T> list){
+//        CommonResult<T> result = new CommonResult<>();
+//
+//
+//    }
+
+}
